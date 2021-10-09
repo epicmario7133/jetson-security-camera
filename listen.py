3a7888566512806d0c0f8678731562920efd4399
EpicMario71 <56398081+epicmario7133@users.noreply.github.com>
2021-10-09 15:51:07 +0200

    Create listen.py

---
+++ listen.py
@@ -0,0 +1,31 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import json
+TOKEN = "Your token here"
+
+
+with open('data.json') as json_file:
+    data = json.load(json_file)
+
+
+def on(update, context):
+    update.message.reply_text('AI has been enabled')
+    data["on"] = "1"
+    with open('data.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+def off(update, context):
+    update.message.reply_text('AI has been disabled')
+    data["on"] = "0"
+    with open('data.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("on", on))
+    dispatcher.add_handler(CommandHandler("off", off))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
