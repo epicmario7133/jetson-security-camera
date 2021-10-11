26944305674a6e8676e17e020685da340775ac36
EpicMario71 <56398081+epicmario7133@users.noreply.github.com>
2021-10-11 18:30:15 +0200

    Create ssd-inception.py

---
+++ ssd-inception.py
@@ -0,0 +1,35 @@
+import jetson.inference
+import jetson.utils
+import time
+import os
+import telegram
+import json
+os.system("gnome-terminal -e 'sh -c \"python3.6 listen.py; exec bash\"'")
+api_key = 'bot_api_key_here'
+user_id = 'user_id_here'
+bot = telegram.Bot(token=api_key)
+bot.send_message(chat_id=user_id, text='Jetson Security Camera started')
+
+net = jetson.inference.detectNet(argv=['--model=ssd-inception-v2', '--input-blob=input_0', '--output-cvg=scores', '--output-bbox=boxes'])
+camera = jetson.utils.videoSource("/dev/video0")
+display = jetson.utils.videoOutput("display://0") #don't change dis
+
+def sendphoto():
+    time.sleep(1)
+    bot.send_message(chat_id=user_id, text='⚠️ Person found ⚠️')
+    bot.send_photo(chat_id=user_id, photo=open('screenshot.png', 'rb'))
+
+while True:
+    with open('data.json') as json_file: #Check on/of from telegram
+        data = json.load(json_file)
+    img = camera.Capture()
+    detections = net.Detect(img)
+    for detection in detections:
+        id = net.GetClassDesc(detection.ClassID)
+        print(id)
+        if id == "person" and data["on"] == "1":
+            os.system("scrot screenshot.png -u")
+            sendphoto()
+            time.sleep(1) #give time to upload photo, set it on 6 if your internet is slow
+    display.Render(img)
+    display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
