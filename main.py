b813333896d95a68a0c782caff16c14ed4c9adbd
EpicMario71 <56398081+epicmario7133@users.noreply.github.com>
2021-11-01 18:10:42 +0100

    Added the internal set-up to the code

--- main.py
+++ main.py
@@ -4,11 +4,32 @@ import time
 import os
 import telegram
 import json
+
+with open('data.json') as json_file: #Check first open and api key
+    data = json.load(json_file)
+
+if data["first-open"] == "0":
+    data["api_key"] = input("""\
+    1) Open telegram
+    2) In the search bar search: @Botfather
+    3) Click “Start”
+    4) Type /newbot and send it
+    5) Select a name for your bot
+    6) Paste the token here
+    """)
+    data["user_id"] = input("""\
+    Open telegram
+    In the search bar search: @userinfobot
+    Click “Start”    
+    Paste the id here
+    """)
+    data["first-open"] = "1"
+    with open('data.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+bot = telegram.Bot(token=data["api_key"])
+bot.send_message(chat_id=data["user_id"], text='Security cam bot start')
 os.system("gnome-terminal -e 'sh -c \"python3.6 listen.py; exec bash\"'")
-api_key = 'bot_api_key_here'
-user_id = 'user_id_here'
-bot = telegram.Bot(token=api_key)
-bot.send_message(chat_id=user_id, text='Jetson Security Camera started')
 
 net = jetson.inference.detectNet(argv=['--model=model/ssd-mobilenet.onnx', '--labels=model/labels.txt', '--input-blob=input_0', '--output-cvg=scores', '--output-bbox=boxes'])
 camera = jetson.utils.videoSource("/dev/video0")
@@ -16,9 +37,9 @@ display = jetson.utils.videoOutput("display://0") #don't change dis
 
 def sendphoto():
     time.sleep(1)
-    bot.send_message(chat_id=user_id, text='⚠️ Person found ⚠️')
-    bot.send_photo(chat_id=user_id, photo=open('screenshot.png', 'rb'))
-
+    bot.send_message(chat_id=data["user_id"], text='⚠️ Person found ⚠️')
+    bot.send_photo(chat_id=data["user_id"], photo=open('screenshot.png', 'rb'))
+    
 while True:
     with open('data.json') as json_file: #Check on/of from telegram
         data = json.load(json_file)
